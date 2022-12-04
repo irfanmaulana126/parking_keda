36867de70dc8e6804e925eadb0e9ce5876128b7b
irfanmaulana126 <irfanmaulana126@gmail.com>
2022-12-04 21:19:22 +0800

    change data type varchar to timestamp

--- database/database.sql
+++ database/database.sql
@@ -3,7 +3,7 @@ CREATE DATABASE parking;
 CREATE TABLE public.parking (
 	id int8 NOT NULL DEFAULT nextval('product_id_seq'::regclass),
 	type_transport varchar NULL,
-	start_date varchar NULL,
-	end_date varchar NULL,
+	start_date timestamp NULL,
+	end_date timestamp NULL,
 	price varchar NULL
 );
